a1cf9d126c4059e32ff3d4178ecb497fcd27e695
jaisai1337 <kjaisai1337@gmail.com>
2022-10-28 15:40:10 +0530

    Chatting App

---
+++ Chatting/app/src/main/java/com/jaiimpu/chatting/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.jaiimpu.chatting.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jaiimpu.chatting.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
